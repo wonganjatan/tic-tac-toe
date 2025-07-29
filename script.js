3780b4d42285abb096440c8a6249a58654cd0341
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-07-29 15:20:16 +1000

    Added little bit of functionality

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ let currentPlayer = player1
 console.log(player1)
 console.log(player2)
 
+function checkWinner(board) {
+    const winCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    return winCombos.some(combo => 
+        combo.every(index => board[index] === currentPlayer.marker))
+}
+
 function playMove(index) {
     if (board[index] !== "") {
         return;
@@ -16,7 +27,13 @@ function playMove(index) {
 
     board[index] = currentPlayer.marker
 
-    currentPlayer = currentPlayer === player1 ? player2 : player1
+    if (checkWinner(board)) {
+        console.log(`${currentPlayer.name} wins`)
+    } else if (board.every(cell => cell !== "")) {
+        console.log("Draw")
+    } else {
+        currentPlayer = currentPlayer === player1 ? player2 : player1
+    }
 }
 
 playMove(0)
